9f54a55e859ff1b4dde87a06161c281cb3adc991
junyoung <huni201917@gmail.com>
2025-06-17 00:18:27 +0900

    0617ai모델링 과제

---
+++ aimodeling_3/5-1.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+print(tf.__version__)
+a=tf.random.uniform([2,3],0,1)
+print(a)
+print(type(a))
+print("최준영-5-1")
--- aimodeling_3/translation_fr.py
+++ aimodeling_3/translation_fr.py
@@ -103,3 +103,49 @@ print('2482018 최준영')
 import numpy as np
 
 encoder_model = Model(inputs=encoder_inputs, outputs=encoder_states)
+decoder_state_input_h = Input(shape=(256, ))
+decoder_state_input_c = Input(shape=(256, ))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+
+decoder_softmax_outputs = decoder_softmax_layer(decoder_outputs)
+decoder_model = Model(inputs=[decoder_inputs] + decoder_states_inputs, outputs=[decoder_softmax_outputs]+ decoder_states)
+
+idx_to_src = dict((i, char) for i, char in src_to_idx.items())
+idx_to_tar = dict((i, char) for i, char in tar_to_idx.items())
+
+def predict_decode(input_seq):
+    states_values = encoder_model.predict(input_seq)
+    target_seq = np.zeros((1,1, tar_vocab_size))
+    target_seq[0, 0, tar_to_idx['\t']] = 1
+    stop = False
+    decoded_sentence = ""
+
+    while not stop:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_values, verbose=0)
+        sampled_token_index = np.argmax(output_tokens[0, 0, :])
+        sampled_char = idx_to_tar[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        if sampled_char == '\n' or len(decoded_sentence) > max_tar_len:
+            stop = True
+
+        target_seq = np.zeros((1,1, tar_vocab_size))
+        target_seq[0, 0, sampled_token_index] = 1.0
+
+        states_values = [h, c]
+
+    return decoded_sentence
+
+
+for seq_index in [0, 40, 300, 1000]:
+    input_seq = encoder_onehot_input[seq_index:seq_index+1]
+    decoded_sentence = predict_decode(input_seq)
+
+    print("입력 : ", lines.src[seq_index])
+
+    print("정답 : ", lines.tar[seq_index][1:len(lines.tar[seq_index])-1])
+    print("번역 : ", decoded_sentence[:len(decoded_sentence)-1],'\n')
+
+print('2482018 최준영')
